4b49d0df0a9a37a9d956418e6c494e8a1bcda60c
Siddharth Khatsuriya <skhasturiya@splunk.com>
2021-08-20 17:19:05 +0530

    trying on SNOW FORK

--- tools/sync.sh
+++ tools/sync.sh
@@ -94,8 +94,8 @@ else
     fi
     git remote set-url origin https://${GH_USER_ADMIN}:${GH_TOKEN_ADMIN}@github.com/$REPOORG/$REPO.git
 
-    ( git checkout test/common-template-rollout-changes${PR_SUFFIX}  && git checkout main && git branch -D test/common-template-rollout-changes${PR_SUFFIX} ) || true
-    git checkout -B "test/common-template-rollout-changes${PR_SUFFIX}" main
+    ( git checkout test/test-do-not-delete-or-merge  && git checkout main && git branch -D test/test-do-not-delete-or-merge ) || true
+    git checkout -B "test/test-do-not-delete-or-merge" main
     git submodule update --init --recursive
 
     rsync -avh --include ".*" --ignore-existing ../../seed/ .
@@ -307,8 +307,8 @@ else
     gh api /repos/$REPOORG/$REPO  -H 'Accept: application/vnd.github.nebula-preview+json' -X PATCH -F visibility=$REPOVISIBILITY
     git add . || exit 1
     git commit -am "test: common template rollout changes" || exit 1
-    git push -f --set-upstream origin test/common-template-rollout-changes${PR_SUFFIX} || exit 1
-    gh pr create \
-        --title "Bump repository configuration from template${PR_SUFFIX}" --fill  || exit 1    
+    git push -f --set-upstream origin test/test-do-not-delete-or-merge || exit 1
+    # gh pr create \
+    #     --title "Bump repository configuration from template${PR_SUFFIX}" --fill  || exit 1    
 fi
 popd
