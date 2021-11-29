c3c96611ffae92e44f97dbfd4ea11dbb54e73011
kkania-splunk <76955023+kkania-splunk@users.noreply.github.com>
2021-11-29 11:55:22 +0100

    feat: branch protection rules (#170)
    * feat: branch protection rules sh
    
    * ci: new job for branch protection rules
    
    * ci: token change
    
    * fix: update bpr only if needed

---
+++ tools/branch_protection_rules.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+REPOORG=splunk
+
+echo "Setting branch protection rules for" "$REPOORG"/"$REPO"
+
+# temporary we need to skip some explicite given repositories and those without pre-publish job
+## SKIP START
+
+declare -a repos_to_be_ommited=("splunk-add-on-for-cyberark-epm")
+
+for repo_to_omit in $repos_to_be_ommited
+do
+    [[ "$REPO" == "$repo_to_omit" ]] && echo "Setting branch protection rules for $REPOORG/$REPO is skipped: repo marked as to be ommitted" && exit
+done
+
+run_id_on_main=$(gh api "repos/$REPOORG/$REPO/actions/runs" -q "[.workflow_runs[] | select (.head_branch==\"main\" and .name==\"build-test-release\")][0] | .id")
+has_pre_publish=$(gh api "repos/$REPOORG/$REPO/actions/runs/$run_id_on_main/jobs" -q 'any(.jobs[].name; . == "pre-publish")')
+[[ "$has_pre_publish" == "false" ]] && echo "Setting branch protection rules for $REPOORG/$REPO is skipped: no pre-publish job on main" && exit
+
+##SKIP STOP
+
+
+create_pattern="mutation createBranchProtectionRule {
+    createBranchProtectionRule(input: {
+        repositoryId: \""%s"\"
+        pattern: \""%s"\"
+        "%s"
+    })
+    {
+        branchProtectionRule {
+            repository { nameWithOwner }
+            pattern
+            creator { login }
+            matchingRefs(last:100) { edges { node { name } } }
+            branchProtectionRuleConflicts(last:10)  { edges { node { conflictingBranchProtectionRule { pattern } } } }
+            requiresApprovingReviews
+            requiredApprovingReviewCount
+            dismissesStaleReviews
+            requiresCodeOwnerReviews
+            restrictsReviewDismissals
+            reviewDismissalAllowances(first:1) { nodes { actor { __typename ... on Team { name } } } }
+            requiresStatusChecks
+            requiresStrictStatusChecks
+            requiredStatusCheckContexts
+            requiresConversationResolution
+            requiresCommitSignatures
+            requiresLinearHistory
+            isAdminEnforced
+            allowsForcePushes
+            allowsDeletions
+        }
+    }
+}"
+
+update_pattern="${create_pattern/repositoryId/branchProtectionRuleId}"
+update_pattern="${update_pattern//createBranchProtectionRule/updateBranchProtectionRule}"
+
+main_settings="requiresApprovingReviews: true
+    requiredApprovingReviewCount: 2
+    dismissesStaleReviews: true
+    requiresCodeOwnerReviews: false
+    restrictsReviewDismissals: true
+    reviewDismissalActorIds: [
+          \""$ADMIN_TEAM_ID"\"
+    ]
+    requiresStatusChecks: true
+    requiresStrictStatusChecks: true
+    requiredStatusCheckContexts: [
+        \"pre-publish\"
+    ]
+    requiresConversationResolution: false
+    requiresCommitSignatures: true
+    requiresLinearHistory: true
+    isAdminEnforced: false
+    restrictsPushes: false
+    allowsForcePushes: false
+    allowsDeletions: false"
+
+develop_settings="${main_settings/allowsDeletions: false/allowsDeletions: true}"
+common_settings="requiresApprovingReviews: false
+    requiresStatusChecks: false
+    requiresConversationResolution: false
+    requiresLinearHistory: false
+    isAdminEnforced: false
+    requiresCommitSignatures: true
+    restrictsPushes: false
+    allowsDeletions: true
+    allowsForcePushes: true"
+
+repo_id="$(gh api "/repos/$REPOORG/$REPO" -q .node_id)"
+
+look_for_branch_protection_rules_template="query {
+    repository(owner:\"${REPOORG}\", name:\"$REPO\") {
+        branchProtectionRules(first:%s) {
+            nodes { id pattern }
+            totalCount
+        }
+    }
+}"
+
+bpr_to_get=20
+look_for_branch_protection_rules=$(printf "$look_for_branch_protection_rules_template" "$bpr_to_get")
+branch_protection_rules=$(gh api graphql -F "query=$look_for_branch_protection_rules")
+number_of_bpr=$(echo $branch_protection_rules | jq '.[] | .repository.branchProtectionRules.totalCount')
+
+if (( $number_of_bpr > $bpr_to_get ))
+then
+  look_for_branch_protection_rules=$(printf "$look_for_branch_protection_rules_template" "$number_of_bpr")
+  branch_protection_rules=$(gh api graphql -F "query=$look_for_branch_protection_rules")
+fi
+
+declare -A branches=( ["main"]="main" ["develop"]="develop" ["common"]="**/**")
+declare -a rule_names=("main" "develop" "common")
+
+for rule_name in "${rule_names[@]}"
+do
+    rule_id=$(echo $branch_protection_rules | jq -r ".[] | .repository.branchProtectionRules.nodes | .[] |  select(.pattern == \""${branches[$rule_name]}"\" )| .id")
+    pattern_name="${rule_name}_settings"
+
+    if [ -z $rule_id ]
+    then
+      echo "no rule for ${rule_name} branch"
+      query=$(printf "$create_pattern" "$repo_id" "${branches[$rule_name]}" "${!pattern_name}")
+    else
+      echo "rule exists for ${rule_name} branch"
+      query=$(printf "$update_pattern" "$rule_id" "${branches[$rule_name]}" "${!pattern_name}")
+    fi
+
+    gh api graphql -F "query=$query"
+done
--- tools/sync.py
+++ tools/sync.py
@@ -4,19 +4,32 @@ import csv
 import os
 import pprint
 import subprocess as sp
-def f(row):
+import argparse
+from itertools import repeat
+
+
+def f(row, script):
     for k in row:
         if row[k]:
             os.environ[k] = row[k]
-    return sp.run("tools/sync.sh",capture_output=True,shell=True)
-    
+    return sp.run(script, capture_output=True, shell=True)
+
+
+def main(script, repositories):
+    inventory = []
+    for repo_csv in repositories:
+        with open(repo_csv, newline='') as csvfile:
+            fields = ['REPO', 'TAID', 'REPOVISIBILITY', 'TITLE', 'BRANCH', 'OTHER']
+            rows = csv.DictReader(csvfile,fieldnames=fields)
+            for r in rows:
+                inventory.append(r)
+    with Pool(processes=2) as p:
+        pprint.pprint(p.starmap(f, zip(inventory, repeat(script))))
+
 
 if __name__ == '__main__':
-    inventory = []
-    with open('repositories_main.csv', newline='') as csvfile:
-        fields = ['REPO', 'TAID', 'REPOVISIBILITY', 'TITLE', 'BRANCH', 'OTHER']
-        rows = csv.DictReader(csvfile,fieldnames=fields)
-        for r in rows:
-            inventory.append(r)
-    with Pool(processes=2) as p:
-        pprint.pprint(p.map(f, inventory))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('script', help='path to script to be run')
+    parser.add_argument('repositories', nargs='*', default=['repositories_main.csv'], help='csv with repositories')
+    args = parser.parse_args()
+    main(script=args.script, repositories=args.repositories)
